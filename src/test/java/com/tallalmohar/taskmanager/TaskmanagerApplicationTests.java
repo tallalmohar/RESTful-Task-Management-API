394737b9e584893719991e979ab8e6edcac31875
tallalmohar <tallalmoohar@gmail.com>
2026-01-06 22:29:06 -0800

    initial commit

---
+++ src/test/java/com/tallalmohar/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallalmohar.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
